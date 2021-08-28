a3600ce03ba286670fb7033d082229a9efb4c3f8
Boris <boris.marinov99@gmail.com>
2021-08-28 12:50:11 +0300

    add gallery view

---
+++ client/src/views/Gallery.vue
@@ -0,0 +1,14 @@
+<template>
+    <Gallery />
+</template>
+
+<script>
+import Gallery from "../components/Gallery"
+
+export default {
+    name: "Gallery",
+    components: {
+        Gallery
+    }
+}
+</script>

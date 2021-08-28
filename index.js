c92dfbf542cbb2f9d46bc9cbd8fcd83b9c30c12d
Boris <boris.marinov99@gmail.com>
2021-08-28 13:13:07 +0300

    fix cors problem

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const ImageController = require('./controllers/ImageController');
 const app = express();
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 mongoose
     .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(function() {
